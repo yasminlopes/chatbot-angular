c05e71f2b233253c79db03279fd4e39fac2d6faf
Yasmin Lopes <yclopes27@gmail.com>
2023-10-17 12:46:52 -0300

    feat: create button component

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent {
+
+  @Input({ required: true }) public text: string;
+  @Input({ required: true }) public icon: string;
+  @Output() public onClick = new EventEmitter();
+}
