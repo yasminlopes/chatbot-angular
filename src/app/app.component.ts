174e4a1d28ce1f8936908c6441bfe3e1ced4beaa
Yasmin Lopes <yclopes27@gmail.com>
2023-10-17 16:52:44 -0300

    feat: set page title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bard-chatbot';
+  title = 'Chatbot with Angular and Bard';
 }
