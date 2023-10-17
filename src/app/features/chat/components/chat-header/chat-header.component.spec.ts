016281f9ef4d4e54bf95798d2bbae6f87c8946fd
Yasmin Lopes <yclopes27@gmail.com>
2023-10-17 15:11:35 -0300

    feat: add chat header

---
+++ src/app/features/chat/components/chat-header/chat-header.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChatHeaderComponent } from './chat-header.component';
+
+describe('ChatHeaderComponent', () => {
+  let component: ChatHeaderComponent;
+  let fixture: ComponentFixture<ChatHeaderComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ChatHeaderComponent]
+    });
+    fixture = TestBed.createComponent(ChatHeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/chat/components/chat-header/chat-header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-chat-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './chat-header.component.html',
+  styleUrls: ['./chat-header.component.scss']
+})
+export class ChatHeaderComponent {
+
+}
